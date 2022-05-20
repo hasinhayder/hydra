c670d6a2fe6cf4cd3f0cd58ea5d99fd9ef7f3955
Hasin Hayder <countdraculla@gmail.com>
2022-05-20 22:22:30 +0600

    Inverse of Relationship - Role belongs to many User

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\User;
 
 class Role extends Model
 {
@@ -16,4 +17,8 @@ class Role extends Model
         'created_at',
         'updated_at',
     ];
+
+    public function users(){
+        return $this->belongsToMany(User::class,'user_roles');
+    }
 }
