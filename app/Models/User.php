c74b3a09872c0cf4c969a9b5a035d190d0ea4a59
Hasin Hayder <hasin@leevio.com>
2022-05-23 00:48:38 +0600

    Hide unnecessary fields in API response

--- app/Models/User.php
+++ app/Models/User.php
@@ -32,6 +32,9 @@ class User extends Authenticatable
     protected $hidden = [
         'password',
         'remember_token',
+        'created_at',
+        'updated_at',
+        'email_verified_at'
     ];
 
     /**
